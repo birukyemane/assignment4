7bbcf2fd7e4d3475070a05b4abad22c12e8fbadd
yemanebiruk <bir_yem@yahoo.com>
2018-10-28 00:35:46 +0300

    added regExp. assigning valid and invalid classes using classList insted of className

--- main.js
+++ main.js
@@ -9,25 +9,25 @@ const button = document.querySelector ('button');
 
 
 const patterns = {
-  fullName: /[A-Za-z' -]{2,50}/,
-  phones: /[0-9]{3}[ -][0-9]{3}[ -][0-9]{4}/,
-  email:/email/,
-  ReEnterEmail: /ReEnterEmail/,
-  address1: /[A-Za-z0-9' -,.]+/,
-  address2: /address2/,
-  city:/city/,
-  state: /state/,
-  country: /country/,
-  zip:/zip/,
-  aboutUS: /aboutUS/,
-  portfolio: /url/,
-  another: /another/
+  fullName: /[A-Za-z' -]{2,50}$/,
+  phones: /[0-9]{3}[ -][0-9]{3}[ -][0-9]{4}$/,
+  email:/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+  ReEnterEmail: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+  address1: /[A-Za-z0-9' -,.]+$/,
+  address2: /[A-Za-z0-9' -,.]+$/,
+  city:/[A-Za-z0-9' -,.]+$/,
+  state: /[A-Za-z0-9' -,.]+$/,
+  country: /[A-Za-z0-9' -,.]+$/,
+  zip:/^\d{5}$/,
+  aboutUS: /.{0,50}$/,
+  portfolio: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/,
+  another: /.{0,50}$/
 };
 
 let arryInputs = Array.from(inputs);
 function allPassed() {
  return arryInputs.every(input=>{
-          return input.className == 'valid'   
+          return input.classList.contains('valid');
   });  
 } 
 
@@ -39,14 +39,18 @@ inputs.forEach (element => {
 
 const formValidator = (element, pattern) => {
   if (pattern.test (element.value)) {
-    element.className = 'valid';
+    //element.className = 'valid';
+      element.classList.add('valid');
+      element.classList.remove('invalid');
     if(allPassed()){
       button.className = 'submit-button';
       button.disabled = false;
     }   
   } else {
-    element.className = 'invalid';
-    button.classList.remove ('submit-button');
-    button.disabled = true;
+    //element.className = 'invalid';
+      element.classList.add('invalid');
+      element.classList.remove('valid');
+      button.classList.remove('submit-button');
+      button.disabled = true;
   }
-};
+}
